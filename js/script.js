88e218ae115f0a424e4070e4cd61ed9a0c018263
Григорий <grigorii_pankratov@mail.ru>
2023-06-05 20:17:58 +0300

    правки №1 в Задание 9

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
 const skills = {
 	data: [
-	{name: "html", level: 50, img_name: "html.svg"},
-	{name: "css", level: 40, img_name: "css.svg"},
-	{name: "python", level: 30, img_name: "python.svg"},
-	{name: "c++", level: 65, img_name: "c++.svg"}
+		{ name: "html", level: 50, img_name: "html.svg" },
+		{ name: "css", level: 40, img_name: "css.svg" },
+		{ name: "python", level: 30, img_name: "python.svg" },
+		{ name: "c++", level: 65, img_name: "c++.svg" }
 	],
 
-	generateList: function(parentElement){
-	this.data.forEach(Element => {
-    const skill_dt = document.createElement('dt');
-	const skill_dd = document.createElement('dd');
-	const skill_div = document.createElement('div');
+	generateList: function (parentElement) {
+		this.data.forEach(Element => {
+			const skill_dt = document.createElement('dt');
+			const skill_dd = document.createElement('dd');
+			const skill_div = document.createElement('div');
 
-	skill_dt.classList.add('skill-item');
-	skill_dd.classList.add('skill-level');
+			skill_dt.classList.add('skill-item');
+			skill_dd.classList.add('skill-level');
 
-	skill_dt.textContent = Element.name;
-    skill_dd.textContent = `${Element.level}%`;
+			skill_dt.textContent = Element.name;
+			skill_div.textContent = `${Element.level}%`;
 
-	skill_dt.style.backgroundImage = `url(img/${Element.img_name})`
+			skill_dt.style.backgroundImage = `url(img/${Element.img_name})`
 
-	skill_div.style.width = `${Element.level}%`;
-	skill_dd.appendChild(skill_div);
+			skill_div.style.width = `${Element.level}%`;
+			skill_dd.appendChild(skill_div);
 
-	skillList.appendChild(skill_dt);
-	skillList.appendChild(skill_dd);
-	});
+			skillList.append(skill_dt, skill_dd);
+		});
 	}
 };
 
