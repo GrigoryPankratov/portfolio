13243ec1c75c6318db6a653cb9da2719204d32de
Григорий <grigorii_pankratov@mail.ru>
2023-06-14 13:58:18 +0300

    сортировка Задание 10 часть 1

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const skills = {
 	],
 
 	generateList: function (parentElement) {
+		parentElement.innerHTML = '';
 		this.data.forEach(Element => {
 			const skill_dt = document.createElement('dt');
 			const skill_dd = document.createElement('dd');
@@ -25,8 +26,75 @@ const skills = {
 
 			skillList.append(skill_dt, skill_dd);
 		});
-	}
+	},
+
+	sortList: function(prop) {
+		const isDescendingOrder = (prop === this.sortProperty) ? !this.isDescendingOrder : false;
+		this.sortProperty = prop;
+		this.isDescendingOrder = isDescendingOrder;
+  
+		this.data.sort(this.getComparer(prop));
+		if (isDescendingOrder) {
+		  this.data.reverse();
+		}
+  
+		this.generateList(skillList);
+	  },
+  
+	  getComparer: function(prop) {
+		return function(a, b) {
+		  switch (prop) {
+			case 'name':
+			  return a[prop].localeCompare(b[prop]);
+			case 'level':
+			  return a[prop] - b[prop];
+			default:
+			  return 0;
+		  }
+		};
+	  },
+  
+	  sortProperty: '', 
+	  isDescendingOrder: false
 };
 
 const skillList = document.querySelector('dl.skill-list');
 skills.generateList(skillList);
+
+const skillSort = document.querySelector('.sort');
+skillSort.addEventListener('click', (e) => {
+	const starget = e.target;
+  	if (starget.nodeName === 'BUTTON') {
+		e.preventDefault();
+		const property = starget.dataset.type;
+		skills.sortList(property);
+	}
+});
+
+function compare() {
+    skills.data.sort(function(a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+
+    return 0;
+ });
+};
+
+function getComparer(prop) {
+    return function(a, b) {
+
+      if (a[prop] < b[prop]) {
+        return -1;
+      }
+      if (a[prop] > b[prop]) {
+        return 1;
+      }
+
+      return 0;
+    };
+  }
+
